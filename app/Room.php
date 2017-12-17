5e737755e8ec1a1042e07be1526d089d300bc67d
nguyentranthinh <nguyentranthinh430@gmail.com>
2017-12-17 16:23:03 +0700

    create relationship model room, booking and book_room

--- app/Room.php
+++ app/Room.php
@@ -8,4 +8,19 @@ class Room extends Model
 {
     protected $fillable = ['name', 'price', 'status', 'description', 'amount_people', 'image1', 'image2', 'image3', 'room_type_id'];
     protected $table = 'rooms';
+
+    public function roomTypes()
+    {
+        return $this->belongsTo('App\RoomType', 'room_type_id');
+    }
+
+    public function bookRoom()
+    {
+        return $this->hasMany('App\BookRoom', 'room_id');
+    }
+
+    public function bookings()
+    {
+        return $this->belongsToMany('App\Booking', 'book_rooms');
+    }
 }
