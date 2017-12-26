01c0295e7e8e10f2f01ce4a48255fd3a3d0897c9
nguyentranthinh <nguyentranthinh430@gmail.com>
2017-12-26 16:04:57 +0700

    fix commit

--- app/RoomType.php
+++ app/RoomType.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class RoomType extends Model
 {
-<<<<<<< HEAD
     protected $table = 'room_types';
     protected $fillable = ['name', 'description'];
 
@@ -14,14 +13,4 @@ class RoomType extends Model
     {
         return $this->hasMany('App\Room', 'room_type_id', 'id');
     }
-=======
-  protected $table = 'room_types';
-  protected $fillable = ['name', 'description'];
-  public $timestamps = false;
-
-  public function rooms()
-  {
-    return $this->hasMany('App\Room',  'room_type_id', 'id');
-  }
->>>>>>> 8f75c41602a7f2a5c25c48b1a5a4c5500d4b8210
 }
